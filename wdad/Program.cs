c251267f3a47a53a32fa2f363bc7504d468acd3e
ElshadMammadov <elshadgm@code.edu.az>
2025-01-20 16:45:39 +0400

    adsda

--- wdad/Program.cs
+++ wdad/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Wdasdsadsaorld!");
+app.MapGet("/", () => "Hello dsdgfvdsasd!");
 
 app.Run();
